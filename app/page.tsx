816facd072fe81a2d22b601f5f841af27da88b02
Elle Brooks <ellebell43@proton.me>
2025-09-15 13:37:23 -0700

    initial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-[8.5in] h-[11in] bg-white shadow p-4">
+      <h1 className="text-6xl font-bold text-center">Floor 10 Garden Party</h1>
+    </div>
+  );
+}
